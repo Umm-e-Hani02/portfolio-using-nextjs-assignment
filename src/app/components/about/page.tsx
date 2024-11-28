ae2d3c8c796551993cc0fa559b26e74c0f3615e9
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-28 21:14:32 +0500

    Update page.tsx

--- src/app/components/about/page.tsx
+++ src/app/components/about/page.tsx
@@ -19,15 +19,15 @@ export default function About() {
         <div className="mt-8 lg:mt-0 lg:text-left text-center max-w-lg">
           <h1 className="text-4xl font-semibold text-yellow-600">Who Am I?</h1>
           <p className="mt-4">
-            Hello! I am Umm-E-Hani, a 14-year-old tech enthusiast and 9th-grade science student with a passion
-            for technology and innovation. I am currently part of the Governor Sindh IT Initiative, where I'm
-            delving into fields like AI, Metaverse, and Web 3.0. My journey in tech started with a curiosity to
-            understand how things work, which has now turned into a dedicated pursuit of skills in
+            Hello! I am Umm-E-Hani a 14-year-old tech enthusiast and 9th-grade science student with a passion
+            for technology and innovation. I am currently part of the Governor Sindh IT Initiative where I am
+            delving into fields like AI Metaverse and Web 3.0. My journey in tech started with a curiosity to
+            understand how things work which has now turned into a dedicated pursuit of skills in
             programming and development.
             <br /><br />
-            Balancing my school studies with my passion, I aim to become a skilled full-stack developer and
-            certified AI expert, using creativity and technology to make a meaningful impact. I believe in
-            constant learning and am excited about all the future holds. Let's connect on LinkedIn or GitHub to
+            Balancing my school studies with my passion I aim to become a skilled full-stack developer and
+            certified AI expert using creativity and technology to make a meaningful impact. I believe in
+            constant learning and am excited about all the future holds. Lets connect on LinkedIn or GitHub to
             follow my journey and share ideas!
           </p>
           <hr className="my-6" />
